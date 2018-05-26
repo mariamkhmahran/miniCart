953ed75e3c0dfc0442b46c056cbeeb6859cad973
mariamkhmahran <mariamkhmahran@hotmail.com>
2018-05-26 07:34:43 +0200

    Fix: get items count from cart service

--- Cart/src/app/app.component.ts
+++ Cart/src/app/app.component.ts
@@ -8,33 +8,15 @@ import * as localForage from 'localforage';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(private cartService: CartService){
-    this.getCount();
-    // TODO: why using setInterval?
-    setInterval(() => {
-      this.getCount();
-    }, 5000);
-  }
+
   title = 'app';
   cartCount: number;
 
-  getCount() {
-    var self = this;
-    // TODO: use the card service
-    localForage.getItem('items').then(function (value) {
-      let items: [any];
-      if(value){
-          items = value as [any];
-      } else {
-          items = [] as [any];
-      }
-      self.cartCount = items.length;
-
-      var count = self.cartCount + "";
+  constructor(private cartService: CartService){
+    this.cartService.newCount.subscribe((value: number) => {
+      this.cartCount = value;
+      var count = this.cartCount + "";
       document.getElementById("cart_btn").setAttribute("data-badge", count);
-
-    }).catch(function (err) {
-        console.log(err);
     });
   }
 
